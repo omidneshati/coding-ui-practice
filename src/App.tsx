2911dca184a875e5a0bef7f05d04d7d4d4bc4257
Omid Neshati <tchandnofame@gmail.com>
2023-04-17 06:56:49 +0330

    insert components to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import FakeChart from "./components/Chart/FakeChart";
+import DetailBoard from "./components/DetailBoard/DetailBoard";
+import Header from "./components/Header/Header";
+import OrderEntry from "./components/OrderEntry/OrderEntry";
+import Sidebar from "./components/Sidebar/Sidebar";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Sidebar />
+      <Header />
+      <FakeChart />
+      <OrderEntry />
+      <DetailBoard />
+    </div>
+  );
 }
 
 export default App;
